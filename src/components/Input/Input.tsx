b7867614c6a1a5119fb3050575fba85e023092a9
Bora Karaman <bkaraman2010@gmail.com>
2024-01-09 18:03:28 +0300

    input commit

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,13 +1,37 @@
-import React from 'react'
+import React, { ReactNode } from 'react'
 
 export interface InputProps {
   className?: string
+  addonAfter?: ReactNode
+  addonBefore?: ReactNode
+  allowClear?: boolean | { clearIcon?: ReactNode }
   bordered?: boolean
+  defaultValue?: string
+  disabled?: boolean
+  id?: string
+  maxLength?: number
+  prefix?: ReactNode
+  showCount?: boolean | { formatter?: (info?: { value?: string; count?: number; maxLength?: number }) => ReactNode }
+  status?: 'error' | 'warning'
+  style?: React.CSSProperties
+  size?: 'large' | 'middle' | 'small'
+  suffix?: ReactNode
+  type?: string
+  value?: string
+  onChange?: React.ChangeEventHandler<HTMLInputElement>
+  onPressEnter?: (event: React.KeyboardEvent<HTMLInputElement>, value?: string) => void
 }
 
 const InternalInput = (props: InputProps) => {
-  console.log('InternalInput', props)
-  return <div>input1</div>
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      // Enter tuşuna basıldığında onPressEnter fonksiyonunu çağır
+      if (props.onPressEnter) {
+        props.onPressEnter(e, e.currentTarget.value)
+      }
+    }
+  }
+  return <input className={props.className} onChange={props.onChange} onKeyDown={handleKeyDown} />
 }
 
 export default InternalInput
--- src/test/input.tsx
+++ src/test/input.tsx
@@ -1,15 +1,19 @@
 import React from 'react'
 import { Input } from '../components/Input'
 
-const { TextArea } = Input;
+const { TextArea } = Input
 
 const input = () => {
-    return (
-        <React.StrictMode>
-            <Input className='abc' />
-            <TextArea autoSize={true} />
-        </React.StrictMode>
-    )
+  return (
+    <React.StrictMode>
+      <Input
+        className='abc'
+        onChange={(e) => console.log('onChange', e)}
+        onPressEnter={(e, a) => console.log('onPressEnter', e, a)}
+      />
+      <TextArea autoSize={true} />
+    </React.StrictMode>
+  )
 }
 
 export default input
